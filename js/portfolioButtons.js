706daf4c4396dd29a99418ef6b57636edc5187a3
ChrisRRadford <cradford@bell.net>
2019-08-13 16:20:03 -0400

    Fixed sketchfab window (kinda) and hid latest work sliders when looking at a portfolio tile

--- js/portfolioButtons.js
+++ js/portfolioButtons.js
@@ -3,83 +3,115 @@
 // Auto Flight plan-Portfolio
 document.getElementById("AutoFP").addEventListener('click', function(){
   document.querySelector('.autoFP-bg-modal').style.display = 'flex';
+  document.querySelector('.glider-prev').style.visibility = 'hidden';
+  document.querySelector('.glider-next').style.visibility = 'hidden';
 });
 
 // Close
 document.getElementById("AutoFP-close").addEventListener('click', function(){
   document.querySelector('.autoFP-bg-modal').style.display = 'none';
+  document.querySelector('.glider-prev').style.visibility = 'visible';
+  document.querySelector('.glider-next').style.visibility = 'visible';
 });
 
 // Change Detection
 document.getElementById("cd").addEventListener('click', function(){
   document.querySelector('.cd-bg-modal').style.display = 'flex';
+  document.querySelector('.glider-prev').style.visibility = 'hidden';
+  document.querySelector('.glider-next').style.visibility = 'hidden';
 });
 
 // Close
 document.getElementById("cd-close").addEventListener('click', function(){
   document.querySelector('.cd-bg-modal').style.display = 'none';
+  document.querySelector('.glider-prev').style.visibility = 'visible';
+  document.querySelector('.glider-next').style.visibility = 'visible';
 });
 
 // Image Stitching
 document.getElementById("imgStitch").addEventListener('click', function(){
   document.querySelector('.imgStitch-bg-modal').style.display = 'flex';
+  document.querySelector('.glider-prev').style.visibility = 'hidden';
+  document.querySelector('.glider-next').style.visibility = 'hidden';
 });
 
 // Close
 document.getElementById("imgStitch-close").addEventListener('click', function(){
   document.querySelector('.imgStitch-bg-modal').style.display = 'none';
+  document.querySelector('.glider-prev').style.visibility = 'visible';
+  document.querySelector('.glider-next').style.visibility = 'visible';
 });
 
 //Algae Detectiom
 document.getElementById("AlgDec").addEventListener('click', function(){
   document.querySelector('.AlgDec-bg-modal').style.display = 'flex';
+  document.querySelector('.glider-prev').style.visibility = 'hidden';
+  document.querySelector('.glider-next').style.visibility = 'hidden';
 });
 
 // Close
 document.getElementById("AlgDec-close").addEventListener('click', function(){
   document.querySelector('.AlgDec-bg-modal').style.display = 'none';
+  document.querySelector('.glider-prev').style.visibility = 'visible';
+  document.querySelector('.glider-next').style.visibility = 'visible';
 });
 
 // Aerial 3D Model
 document.getElementById("A3DM").addEventListener('click', function(){
   document.querySelector('.A3DM-bg-modal').style.display = 'flex';
+  document.querySelector('.glider-prev').style.visibility = 'hidden';
+  document.querySelector('.glider-next').style.visibility = 'hidden';
 });
 
 // Close
 document.getElementById("A3DM-close").addEventListener('click', function(){
   document.querySelector('.A3DM-bg-modal').style.display = 'none';
+  document.querySelector('.glider-prev').style.visibility = 'visible';
+  document.querySelector('.glider-next').style.visibility = 'visible';
 });
 
 
 // University Course Website
 document.getElementById("UCW").addEventListener('click', function(){
   document.querySelector('.UCW-bg-modal').style.display = 'flex';
+  document.querySelector('.glider-prev').style.visibility = 'hidden';
+  document.querySelector('.glider-next').style.visibility = 'hidden';
 });
 
 // Close
 document.getElementById("UCW-close").addEventListener('click', function(){
   document.querySelector('.UCW-bg-modal').style.display = 'none';
+  document.querySelector('.glider-prev').style.visibility = 'visible';
+  document.querySelector('.glider-next').style.visibility = 'visible';
 });
 
 // Independent Research Paper
 document.getElementById("RP").addEventListener('click', function(){
   document.querySelector('.resPep-bg-modal').style.display = 'flex';
+  document.querySelector('.glider-prev').style.visibility = 'hidden';
+  document.querySelector('.glider-next').style.visibility = 'hidden';
 });
 
 // Close
 document.getElementById("resPep-close").addEventListener('click', function(){
   document.querySelector('.resPep-bg-modal').style.display = 'none';
+  document.querySelector('.glider-prev').style.visibility = 'visible';
+  document.querySelector('.glider-next').style.visibility = 'visible';
 });
 
 
 // Journal Article
 document.getElementById("JA").addEventListener('click', function(){
   document.querySelector('.JA-bg-modal').style.display = 'flex';
+  document.querySelector('.glider-prev').style.visibility = 'hidden';
+  document.querySelector('.glider-next').style.visibility = 'hidden';
 });
 
 // Close
 document.getElementById("JA-close").addEventListener('click', function(){
   document.querySelector('.JA-bg-modal').style.display = 'none';
+  document.querySelector('.glider-prev').style.visibility = 'visible';
+  document.querySelector('.glider-next').style.visibility = 'visible';
 });
 
 
@@ -89,14 +121,21 @@ document.getElementById("JA-close").addEventListener('click', function(){
 // Independent Research Paper Latest Work Section
 document.getElementById("LW-RP").addEventListener('click', function(){
   document.querySelector('.resPep-bg-modal').style.display = 'flex';
+  document.querySelector('.glider-prev').style.visibility = 'hidden';
+  document.querySelector('.glider-next').style.visibility = 'hidden';
 });
 
 // Journal Article Latest Work Section
 document.getElementById("LW-JA").addEventListener('click', function(){
   document.querySelector('.JA-bg-modal').style.display = 'flex';
+  document.querySelector('.glider-prev').style.visibility = 'hidden';
+  document.querySelector('.glider-next').style.visibility = 'hidden';
 });
 
 //Auto Flight plan- latest Work
 document.getElementById("LW-AutoFP").addEventListener('click', function(){
   document.querySelector('.autoFP-bg-modal').style.display = 'flex';
+  document.querySelector('.glider-prev').style.visibility = 'hidden';
+  document.querySelector('.glider-next').style.visibility = 'hidden';
+
 });
