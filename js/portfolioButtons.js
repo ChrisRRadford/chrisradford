89756061906022dbbed7a727f8a69283ea3fc0f0
ChrisRRadford <cradford@bell.net>
2019-08-13 15:09:32 -0400

    fixed div for mobile

--- js/portfolioButtons.js
+++ js/portfolioButtons.js
@@ -1,15 +1,11 @@
 //------------Modal Pop up open and close buttona------------//
 
-//Auto Flight plan- latest Work
-document.getElementById("LW-AutoFP").addEventListener('click', function(){
-  document.querySelector('.autoFP-bg-modal').style.display = 'flex';
-});
-
 // Auto Flight plan-Portfolio
 document.getElementById("AutoFP").addEventListener('click', function(){
   document.querySelector('.autoFP-bg-modal').style.display = 'flex';
 });
 
+// Close
 document.getElementById("AutoFP-close").addEventListener('click', function(){
   document.querySelector('.autoFP-bg-modal').style.display = 'none';
 });
@@ -19,6 +15,7 @@ document.getElementById("cd").addEventListener('click', function(){
   document.querySelector('.cd-bg-modal').style.display = 'flex';
 });
 
+// Close
 document.getElementById("cd-close").addEventListener('click', function(){
   document.querySelector('.cd-bg-modal').style.display = 'none';
 });
@@ -28,6 +25,7 @@ document.getElementById("imgStitch").addEventListener('click', function(){
   document.querySelector('.imgStitch-bg-modal').style.display = 'flex';
 });
 
+// Close
 document.getElementById("imgStitch-close").addEventListener('click', function(){
   document.querySelector('.imgStitch-bg-modal').style.display = 'none';
 });
@@ -37,6 +35,7 @@ document.getElementById("AlgDec").addEventListener('click', function(){
   document.querySelector('.AlgDec-bg-modal').style.display = 'flex';
 });
 
+// Close
 document.getElementById("AlgDec-close").addEventListener('click', function(){
   document.querySelector('.AlgDec-bg-modal').style.display = 'none';
 });
@@ -46,6 +45,7 @@ document.getElementById("A3DM").addEventListener('click', function(){
   document.querySelector('.A3DM-bg-modal').style.display = 'flex';
 });
 
+// Close
 document.getElementById("A3DM-close").addEventListener('click', function(){
   document.querySelector('.A3DM-bg-modal').style.display = 'none';
 });
@@ -56,6 +56,7 @@ document.getElementById("UCW").addEventListener('click', function(){
   document.querySelector('.UCW-bg-modal').style.display = 'flex';
 });
 
+// Close
 document.getElementById("UCW-close").addEventListener('click', function(){
   document.querySelector('.UCW-bg-modal').style.display = 'none';
 });
@@ -65,12 +66,7 @@ document.getElementById("RP").addEventListener('click', function(){
   document.querySelector('.resPep-bg-modal').style.display = 'flex';
 });
 
-// Independent Research Paper Latest Work Section
-document.getElementById("LW-RP").addEventListener('click', function(){
-  document.querySelector('.resPep-bg-modal').style.display = 'flex';
-});
-
-
+// Close
 document.getElementById("resPep-close").addEventListener('click', function(){
   document.querySelector('.resPep-bg-modal').style.display = 'none';
 });
@@ -81,12 +77,26 @@ document.getElementById("JA").addEventListener('click', function(){
   document.querySelector('.JA-bg-modal').style.display = 'flex';
 });
 
+// Close
+document.getElementById("JA-close").addEventListener('click', function(){
+  document.querySelector('.JA-bg-modal').style.display = 'none';
+});
+
+
+
+// ---------------LATEST WORK BUTRTONS
+
+// Independent Research Paper Latest Work Section
+document.getElementById("LW-RP").addEventListener('click', function(){
+  document.querySelector('.resPep-bg-modal').style.display = 'flex';
+});
+
 // Journal Article Latest Work Section
 document.getElementById("LW-JA").addEventListener('click', function(){
   document.querySelector('.JA-bg-modal').style.display = 'flex';
 });
 
-
-document.getElementById("JA-close").addEventListener('click', function(){
-  document.querySelector('.JA-bg-modal').style.display = 'none';
+//Auto Flight plan- latest Work
+document.getElementById("LW-AutoFP").addEventListener('click', function(){
+  document.querySelector('.autoFP-bg-modal').style.display = 'flex';
 });
